59856c1c5390cd78dab0d8eea1fefa4f4edb9926
duongasinma <duongasinma@gmail.com>
2022-10-08 13:37:03 +0700

    Fix OrderModel

--- DeliveryVHGP-WebApi/ViewModels/OrderModels.cs
+++ DeliveryVHGP-WebApi/ViewModels/OrderModels.cs
@@ -3,13 +3,13 @@
     public class OrderModels
     {
         public string Id { get; set; } = null!;
-        public string? Type { get; set; }
         public double? Total { get; set; }
         public string? BuildingId { get; set; }
-        public string? DurationId { get; set; }
         public string? StoreId { get; set; }
         public string? StoreName { get; set; }
-        public string? Status { get; set; }
-        public List<OrderDetailDto> listProInMenuOrder { get; set; }
+        public string? BuildingName { get; set; }
+        public string? StatusId { get; set; }
+        public string? StatusName { get; set; }
+        public string? Time { get; set; }
     }
 }
