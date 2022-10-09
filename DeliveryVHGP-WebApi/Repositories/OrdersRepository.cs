7aee1ba58f0e328c2f46702611c5f76f534c66d3
Tamm <siusaochemgio@gmail.com>
2022-10-09 21:50:23 +0700

    fix getDetail

--- DeliveryVHGP-WebApi/Repositories/OrdersRepository.cs
+++ DeliveryVHGP-WebApi/Repositories/OrdersRepository.cs
@@ -69,11 +69,14 @@ namespace DeliveryVHGP_WebApi.Repositories
             var listPro = await (from o in context.Orders
                                  join odd in context.OrderDetails on o.Id equals odd.OrderId
                                  join pm in context.ProductInMenus on odd.ProductInMenuId equals pm.Id
+                                 join p in context.Products on pm.ProductId equals p.Id
                                  where o.Id == order.Id
                                  select new OrderDetailDto
                                  {
                                      ProductInMenuId = pm.Id,
-                                     Quantity = odd.Quantity
+                                     Price = pm.Price,
+                                     Quantity = odd.Quantity,
+                                     ProductName = p.Name,
 
                                  }).ToListAsync();
             order.ListProInMenu = listPro;
