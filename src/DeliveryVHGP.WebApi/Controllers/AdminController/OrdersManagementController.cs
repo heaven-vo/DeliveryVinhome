a173b2e1cbb45709ca54ccd596e9fba3d570019e
Tamm <siusaochemgio@gmail.com>
2022-10-22 17:28:41 +0700

    get order for admin , filter

--- src/DeliveryVHGP.WebApi/Controllers/AdminController/OrdersManagementController.cs
+++ src/DeliveryVHGP.WebApi/Controllers/AdminController/OrdersManagementController.cs
@@ -12,14 +12,16 @@ namespace DeliveryVHGP.WebApi.Controllers
         {
             this.repository = repository;
         }
-
+        /// <summary>
+        /// Get list order with pagination
+        /// </summary>
         // GET: api/Orders
-        [HttpGet("{CusId}/Customer")]
-        public async Task<ActionResult> GetOrder(string CusId,int pageIndex, int pageSize)
+        [HttpGet]
+        public async Task<ActionResult> GetOrder(int pageIndex, int pageSize)
         {
             try
             {
-                var result = Ok(await repository.Order.GetListOrders(CusId,pageIndex, pageSize));
+                var result = Ok(await repository.Order.GetAll(pageIndex, pageSize));
 
                 return Ok(result);
             }
@@ -27,6 +29,24 @@ namespace DeliveryVHGP.WebApi.Controllers
             {
                 return Conflict();
             }
+        }
+        /// <summary>
+        /// Get list all order by payment with pagination
+        /// </summary>
+        //GET: api/v1/OrderByPayment?pageIndex=1&pageSize=3
+        [HttpGet("search-payment")]
+        public async Task<ActionResult> GetListOrderByPayment(string paymentType, int pageIndex, int pageSize)
+        {
+            return Ok(await repository.Order.GetOrderByPayment(paymentType, pageIndex, pageSize));
+        }
+        /// <summary>
+        /// Get list all order by status with pagination
+        /// </summary>
+        //GET: api/v1/orderByStatus?pageIndex=1&pageSize=3
+        [HttpGet("search-status")]
+        public async Task<ActionResult> GetListOrderByStatus(string statusName, int pageIndex, int pageSize)
+        {
+            return Ok(await repository.Order.GetOrderByStatus(statusName, pageIndex, pageSize));
         }
         /// <summary>
         /// Get product by id with pagination
