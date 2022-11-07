b43fd2b82201d4ea13b7e2b7b52daee52b4e6c89
duongasinma <duongasinma@gmail.com>
2022-11-07 18:17:30 +0700

    Fix deploy

--- src/DeliveryVHGP.Core/Models/PagingRequest.cs
+++ src/DeliveryVHGP.Core/Models/PagingRequest.cs
@@ -9,7 +9,7 @@ namespace DeliveryVHGP.Core.Models
     public class PagingRequest
     {
         public int page { get; set; } = 1;
-        public int pageSize { get; set; } = 10;
+        public int pageSize { get; set; } = 8;
         public string keySearch { get; set; } = "";
         public string searchBy { get; set; } = "";
         //public SortOrder SortType { get; set; } = SortOrder.Descending;
